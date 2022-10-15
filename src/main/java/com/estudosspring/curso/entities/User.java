a6b8c3eeda37f15b77bb70fe458b57f12f906e92
czarframe <cesaragstincs@gmail.com>
2022-10-15 15:30:08 -0300

    Banco de dados H2; Perfil de Teste; JPA

--- src/main/java/com/estudosspring/curso/entities/User.java
+++ src/main/java/com/estudosspring/curso/entities/User.java
@@ -2,13 +2,27 @@ package com.estudosspring.curso.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
+    
     private String telefone;
+    
     private String senha;
 
     public User(){
