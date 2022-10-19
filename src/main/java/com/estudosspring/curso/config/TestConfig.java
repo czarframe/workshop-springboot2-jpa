a9c6d656edf80fa5ed747f6c3c52560f5e3a9eed
czarframe <cesaragstincs@gmail.com>
2022-10-19 11:30:01 -0300

    Product

--- src/main/java/com/estudosspring/curso/config/TestConfig.java
+++ src/main/java/com/estudosspring/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.estudosspring.curso.entities.Category;
 import com.estudosspring.curso.entities.Order;
+import com.estudosspring.curso.entities.Product;
 import com.estudosspring.curso.entities.User;
 import com.estudosspring.curso.entities.enums.OrderStatus;
 import com.estudosspring.curso.repositories.CategoryRepository;
 import com.estudosspring.curso.repositories.OrderRepository;
+import com.estudosspring.curso.repositories.ProductRepository;
 import com.estudosspring.curso.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
     
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
     
     @Override
     public void run(String... args) throws Exception {
@@ -35,7 +40,14 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books"); 
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, ""); 
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, ""); 
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, ""); 
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+
         categoryRepository.saveAll(Arrays.asList(cat1, cat2 , cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
         User u1 = new User(null, "Maria Marrom", "maria@gmail.com", "98888888888", "12345");
         User u2 = new User(null, "Alex Verde", "alex@gmail.com", "97777777777", "12345");
