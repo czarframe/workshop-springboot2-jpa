bd3ae22ce1eda95868bf23e891dfff7b45a222cc
czarframe <cesaragstincs@gmail.com>
2023-04-18 09:26:31 -0300

    Entidade Payment Associação um para um

--- src/main/java/com/estudosspring/curso/config/TestConfig.java
+++ src/main/java/com/estudosspring/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudosspring.curso.entities.Category;
 import com.estudosspring.curso.entities.Order;
 import com.estudosspring.curso.entities.OrderItem;
+import com.estudosspring.curso.entities.Payment;
 import com.estudosspring.curso.entities.Product;
 import com.estudosspring.curso.entities.User;
 import com.estudosspring.curso.entities.enums.OrderStatus;
@@ -79,5 +80,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
         ordemItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2023-02-20T20:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
